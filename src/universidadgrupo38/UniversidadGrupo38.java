797c5ac33d4079a36840244a0359c623dcd662c7
AMAX2023 <juancasco33@gmail.com>
2023-09-15 12:42:42 -0300

    Revert "busca por dni"
    This reverts commit be9be03fa9d6b3b0add53b33e8b9bbb41a74a406.

--- src/universidadgrupo38/UniversidadGrupo38.java
+++ src/universidadgrupo38/UniversidadGrupo38.java
@@ -34,8 +34,7 @@ public class UniversidadGrupo38 {
         //alu.guardarAlumno(juan);
         //alu.modificarAlumno(juan);
         //alu.eliminarAlumno(11);
-        //Alumno alumnoEncontrado=alu.buscarAlumno(23);
-        Alumno alumnoEncontrado=alu.buscarAlumnoPorDni(30825852);
+        Alumno alumnoEncontrado=alu.buscarAlumno(10);
        
         System.out.println("DNI: "+alumnoEncontrado.getDni());
         System.out.println("Apellido: "+alumnoEncontrado.getApellido());
